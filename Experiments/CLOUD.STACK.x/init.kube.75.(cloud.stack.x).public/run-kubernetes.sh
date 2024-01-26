9ef3ec64c7767b9ad8cc76bd16de1529fe314bd7
KOSH VORLON <vorlon@iblog.pro>
2024-01-26 23:40:21 +0500

    add cloud.stack.x.75

---
+++ Experiments/CLOUD.STACK.x/init.kube.75.(cloud.stack.x).public/run-kubernetes.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/bash
+
+function throw()
+{
+   errorCode=$?
+   echo "Error: ($?) LINENO:$1"
+   exit $errorCode
+}
+
+function check_error {
+  if [ $? -ne 0 ]; then
+    echo "Error: ($?) LINENO:$1"
+    exit 1
+  fi
+}
+
+export DEBIAN_FRONTEND=noninteractive
+
+ansible  -i node-hosts-all.yaml --become -m shell -a 'uptime' kubernetes || throw ${LINENO}
+ANSIBLE_LOG_PATH=./ansible.init-kube-000.log ansible-playbook -i  node-hosts-all.yaml playbook-kubernetes/init-kube-000.yaml --extra-vars "@run-kubernetes.vars.yaml" || throw ${LINENO}
+ANSIBLE_LOG_PATH=./ansible.init-kube-001.log ansible-playbook -i  node-hosts-all.yaml playbook-kubernetes/init-kube-001.yaml --extra-vars "@run-kubernetes.vars.yaml" || throw ${LINENO}
+ANSIBLE_LOG_PATH=./ansible.init-kube-002.log ansible-playbook -i  node-hosts-all.yaml playbook-kubernetes/init-kube-002.yaml --extra-vars "@run-kubernetes.vars.yaml" || throw ${LINENO}
+ANSIBLE_LOG_PATH=./ansible.init-kube-003.log ansible-playbook -i  node-hosts-all.yaml playbook-kubernetes/init-kube-003.yaml --extra-vars "@run-kubernetes.vars.yaml" || throw ${LINENO}
+
+ANSIBLE_LOG_PATH=./ansible.init-kube-004.log ansible-playbook -i  node-hosts-all.yaml playbook-kubernetes/init-kube-004.yaml --extra-vars "@run-kubernetes.vars.yaml" || throw ${LINENO}
+ANSIBLE_LOG_PATH=./ansible.init-kube-005.log ansible-playbook -i  node-hosts-all.yaml playbook-kubernetes/init-kube-005.yaml --extra-vars "@run-kubernetes.vars.yaml" || throw ${LINENO}
+ANSIBLE_LOG_PATH=./ansible.init-kube-006.log ansible-playbook -i  node-hosts-all.yaml playbook-kubernetes/init-kube-006.yaml --extra-vars "@run-kubernetes.vars.yaml" || throw ${LINENO}
+ANSIBLE_LOG_PATH=./ansible.init-kube-007.log ansible-playbook -i  node-hosts-all.yaml playbook-kubernetes/init-kube-007.yaml --extra-vars "@run-kubernetes.vars.yaml" || throw ${LINENO}
+
+
+
+# ansible  -i node-hosts-all.yaml --become -m shell -a 'mkdir -p /opt/local-path-provisioner' kubernetes
+# ansible  -i node-hosts-all.yaml --become -m shell -a 'chmod 777 /opt/local-path-provisioner' kubernetes
