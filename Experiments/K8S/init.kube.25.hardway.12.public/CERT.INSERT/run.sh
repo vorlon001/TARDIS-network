eb39f33f57f91d5ccf06d425cdc898f1a3c5d7b2
KOSH VORLON <vorlon@iblog.pro>
2024-02-06 01:25:10 +0500

    remove k8s w/o master nodes v.12

--- Experiments/K8S/init.kube.25.hardway.12.public/CERT.INSERT/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cp ./*.crt  /usr/local/share/ca-certificates/
-update-ca-certificates
-
--- Experiments/K8S/init.kube.25.hardway.12.public/init.020.init.calico.node180.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-
-cd /root
-cd ./CNI/CALICO/3.26.1
-./run.ipv4.sh
-cd /root
